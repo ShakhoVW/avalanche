9d9e9fba49190b44a5ac0f53af58573801c4bae2
Shakhov <shakhov@haulmont.com>
2017-04-01 03:16:35 +0300

    add webpack babel exlint configs update dependencies

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    context: path.resolve(__dirname, './src'),
+    entry: {
+        app: './index.jsx',
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: "bundle.js",
+//        publicPath: "/assets/" // for webpack-dev-server output
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/],
+                use: [{
+                    loader: 'babel-loader',
+                    options: { presets: ['es2015'] }
+                }],
+            },
+            {
+                test: /\.s?css$/,
+                loaders: ExtractTextPlugin.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ],
+                    fallback: 'style-loader'
+                })
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.json'],
+        alias: {
+            src: path.resolve(__dirname, 'src')
+        }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './index.html'
+        }),
+        new ExtractTextPlugin('[name].css'),
+      ],
+    devtool: "eval",
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+        port: 9000
+    }
+};
+
+module.exports = config;
